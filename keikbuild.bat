acfa46f29cfa2fd92d4eda2d2219a537348bc289
fogwizard <jwf1990@126.com>
2020-01-21 22:39:29 +0800

    Add keilbuild.bat to build from command line

---
+++ keikbuild.bat
@@ -0,0 +1,8 @@
+@echo off
+set UV=C:\Keil_v5\UV4\UV4.exe
+set UV_PRO_PATH=%cd%\dp_transmitter.uvprojx
+echo >build_log.txt
+echo start building...
+START /WAIT %UV% -b -j0 %UV_PRO_PATH% -o %cd%\build_log.txt
+type build_log.txt
+echo Done.
