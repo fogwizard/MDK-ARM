9d8aa769b19e529d70fd487cdb8b0ad44af8b9ba
fogwizard <jwf1990@126.com>
2020-01-21 22:44:00 +0800

    update keilbuild.bat

--- keikbuild.bat
+++ keikbuild.bat
@@ -1,6 +1,6 @@
 @echo off
 set UV=C:\Keil_v5\UV4\UV4.exe
-set UV_PRO_PATH=%cd%\dp_transmitter.uvprojx
+set UV_PRO_PATH=%cd%\%1
 echo >build_log.txt
 echo start building...
 START /WAIT %UV% -b -j0 %UV_PRO_PATH% -o %cd%\build_log.txt
